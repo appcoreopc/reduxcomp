0e4c64c3cd4e0bef08acb737965ead27d8c1c4b3
jeremy <kepung@gmail.com>
2016-05-05 18:15:19 +0800

    fix issue

---
+++ components/HelloComponent.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react'
+
+class HelloComponent extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <p>Hello World 1</p>
+    )
+  }
+}
+
+export default HelloComponent
